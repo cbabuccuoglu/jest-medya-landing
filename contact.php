2af4a8f20172f2a0a605a39679ae1f879ba17549
Çağrı Babuçcuoğlu <cagribabuccuoglu@gmail.com>
2020-09-26 17:29:24 +0300

    contact php fixed

--- contact.php
+++ contact.php
@@ -77,5 +77,43 @@
 <!-- end contact -->
 </section>
 <!-- end content -->
-<script src="js/contact.form.min.js"></script>
-<?php include "./elements/footer.php"; ?>
+</main>
+<div class="footer-spacing"></div>
+<!-- end footer-spacing -->
+<footer class="footer"> <img src="images/logo.png" alt="Image">
+    <ul class="social-media">
+        <li><a href="https://www.facebook.com/jestmedyareklam/">FB</a></li>
+        <li><a href="https://instagram.com/jestmedya06">IG</a></li>
+        <li><a href="https://github.com/cbabuccuoglu">Git</a></li>
+    </ul>
+    <div class="footer-bar"> <span class="pull-left">© 2020 Jest</span></div>
+    <!-- end footer-bar -->
+</footer>
+<!-- end footer -->
+
+<!-- JS FILES --> 
+<script src="js/jquery.min.js"></script> 
+<script>
+	// PRELOADER
+		(function($) {
+			$(window).load(function(){
+				$("body").addClass("page-loaded");	
+			});
+		})(jQuery)
+</script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/swiper.min.js"></script> 
+<script src="js/fancybox.min.js"></script> 
+<script src="js/jquery.stellar.js"></script> 
+<script src="js/odometer.min.js"></script> 
+<script src="js/hamburger.min.js"></script> 
+<script src="js/easings.js"></script> 
+<script src="js/isotope.min.js"></script> 
+<script src="js/wow.min.js"></script> 
+<script src="js/perspective.min.js"></script> 
+<script src="js/scripts.js"></script> 
+<script src="js/contact.form.min.js"></script> 
+<script src="js/jquery.form.min.js"></script> 
+<script src="js/jquery.validate.min.js"></script> 
+</body>
+</html>

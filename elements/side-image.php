e80907a56a8b8416e537b697d9041d861d756109
Çağrı Babuçcuoğlu <cagribabuccuoglu@gmail.com>
2020-10-01 16:01:06 +0300

    gif added to side-image element

--- elements/side-image.php
+++ elements/side-image.php
@@ -1,6 +1,6 @@
 <div class="side-image-content">
       <div class="inner">
-        <figure class="reveal-effect se2-white wow main-image" data-aos="slide-effect"><img src="images/image01.jpg" alt="Image"></figure>
+        <figure class="reveal-effect se2-white wow main-image" data-aos="slide-effect"><img src="images/jest.gif" alt="Image"></figure>
         <figure class="reveal-effect se2-white wow sub-image" data-aos="slide-effect"><img src="images/image02.jpg" alt="Image"></figure>
       </div>
       <!-- end inner -->
